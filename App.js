80abc76263079105d9cd88c231053a3fdfaf81bf
Wassim Sayad <wasayad@z3r4p5.42lyon.fr>
2024-04-18 16:16:04 +0200

    tg morgeneneeeeee

--- App.js
+++ App.js
@@ -1,56 +1,184 @@
-import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions, FlatList, Image, TouchableOpacity } from 'react-native';
+import { ProgressBarAndroid } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { useState } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { useEffect } from 'react';
+import beluga from "./assets/beluga.jpg"
 import axios from 'axios';
 
 const { width } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
 const Tab = createBottomTabNavigator();
 
-const Screen1 = ({token}) => {
+const Screen1 = ({ token }) => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [users, setUsers] = useState([]);
+  const navigation = useNavigation();
+
+  const selectProfile = (user, token) => {
+    navigation.navigate('Settings', { user: user, token: token });
+  };
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.item} onPress={() => selectProfile(item, token)}>
+      {
+        item.image.link ?
+          <Image style={styles.image} source={{ uri: item.image.link }} ></Image> :
+          <Image style={styles.image} source={beluga}></Image>
+      }
+      <Text style={styles.itemText}>{item.login}</Text>
+    </TouchableOpacity>
+  );
 
   useEffect(() => {
-    console.log(`Bearer ${token}`);
-    if (searchQuery != '')
-      axios.get(`https://api.intra.42.fr/v2/users?range[login]=${searchQuery},${searchQuery}zzzzz`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }).then((response) => {
-        console.log(response);
-      }).catch( (error) => {
-        console.log(error);
-      });
+    if (searchQuery != '') {
+      const delayDebounceFn = setTimeout(() => {
+        axios.get(`https://api.intra.42.fr/v2/users?range[login]=${searchQuery},${searchQuery}zzzzz`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((response) => {
+          setUsers(response.data)
+        }).catch((error) => {
+          console.log(error);
+        });
+      }, 1500)
+      return () => clearTimeout(delayDebounceFn)
+    }
+    else {
+      setUsers([]);
+    }
   }, [searchQuery]);
-return (
-  <View style={styles.container}>
-    <TextInput
-      style={styles.input}
-      placeholder="Search"
-      value={searchQuery}
-      onChangeText={setSearchQuery}
-    />
-  </View>
-);
-};
 
-const Screen2 = () => {
+
   return (
     <View style={styles.container}>
-      <Text>Screen 2</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Search"
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+      />
+      <FlatList
+      data={users}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
+      />
     </View>
   );
 };
 
-const AppNavigator = ({token}) => {
-  console.log('AppNavigator =', token);
+
+
+
+
+
+const Screen2 = ({ route }) => {
+  const [user, setUser] = useState("");
+  const [cursus, setCursus] = useState(0);
+  const  token  = route.params.token;
+
+  const renderItemCursus = ({ item }) => (
+    <View style={styles.itemProfile}>
+      <Text style={styles.itemText}>{item.name + ": " + item.level}</Text>
+      <ProgressBarAndroid
+      style={styles.progressBar}
+        styleAttr="Horizontal"
+        indeterminate={false}
+        progress={item.level / 21}
+        color="#2196F3"
+      />
+    </View>
+  );
+  const renderItemProject = ({ item }) => (
+    <View style={styles.itemProfile}>
+      <Text style={styles.itemText}>{item.project.name}</Text>
+    </View>
+  );
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemProfile}>
+      {item.label ? <Text style={styles.itemText}>{item.label}</Text> : null}
+      <Text style={styles.itemText}>{item.data}</Text>
+    </View>
+  );
+  // const baseInfo = (user) => {
+  //    return <View>
+  //       <Text>{user.login}</Text>
+  //       <Text>{user.email}</Text>
+  //       <Text>{"correction point:" + user.correction_point}</Text>
+  //       <Text>{"wallet: " + user.wallet}</Text>
+  //    </View>
+  // };
+  useEffect(() => {
+    axios.get(`https://api.intra.42.fr/v2/users/${route.params.user.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((response) => {
+        setUser(response.data);
+        for (let i = 0; i < 5; i++) {
+          if (response.data.cursus_users[i] && response.data.cursus_users[i].grade === "Member")
+          {
+            setCursus(i);
+            break;
+          }
+        }
+      }).catch((error) => {
+        console.log(error);
+      });
+  }, [])
+  return (
+      <View style={styles.container}>
+        {
+        user != "" ?
+        <View style={styles.container}>
+        <Image style={styles.profileImage} source={
+          user.image.link ?
+            { uri: user.image.link } :
+            beluga
+        }/>
+        <FlatList
+        style={styles.list}
+          data={[
+            { label: null, data:  "correction point: " +  user.correction_point },
+            { label: null, data: user.login },
+            { label: null, data: "wallet: " + user.wallet },
+            { label: null, data: user.email }
+          ]}
+          renderItem={renderItem}
+          numColumns={1}
+        />
+        <Text> Skills</Text>
+        <FlatList
+        style={styles.list}
+          data={user.cursus_users[cursus].skills}
+          renderItem={renderItemCursus}
+          numColumns={1}
+        />
+        <Text> Project</Text>
+        <FlatList
+        style={styles.list}
+          data={user.projects_users}
+          renderItem={renderItemProject}
+          numColumns={1}
+        />
+        </View>
+        : null 
+    }
+    </View>
+    
+    
+  );
+};
+
+const AppNavigator = ({ token }) => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" children={()=><Screen1 token={token}/>}/>
+        <Tab.Screen name="Home" children={() => <Screen1 token={token} />} />
         <Tab.Screen name="Settings" component={Screen2} />
       </Tab.Navigator>
     </NavigationContainer>
@@ -60,10 +188,6 @@ const AppNavigator = ({token}) => {
 export default function App() {
   const [code, setCode] = useState('');
   const [token, setToken] = useState('');
-
-  // useEffect(() => {
-  //   console.log(token);
-  // }, [token]);
 
   const GenerateWebView = () => {
     return <WebView
@@ -114,4 +238,41 @@ const styles = StyleSheet.create({
     marginTop: 16,
     paddingLeft: 8,
   },
+  item: {
+    backgroundColor: '#f9c2ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
+    margin: 5,
+    width: width / 2.2, // Subtracting some margin
+  },
+  progressBar: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width * 0.7,
+  },
+  itemProfile: {
+    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 35,
+    margin: 5,
+    width: width  * 0.8, // Subtracting some margin
+  },
+  itemText: {
+    fontSize: 18,
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  profileImage: {
+    width: width,
+    height: height/ 5,
+  },
+  list: {
+
+    height: height / 4,
+    margin:10,
+  }
 });
